7be0239edbde05a451318a98af23a0878f034625
nous <nous@artixlinux.org>
2020-01-22 11:23:04 +0000

    tune buildISO.sh

--- buildISOs.sh
+++ buildISOs.sh
@@ -89,11 +89,11 @@ for profile in ${profiles[@]}; do
         stamp=$(timestamp)
         echo "$stamp == Begin building    ${_branch} $profile ISO with $init" >> $CWD/ISO_build.log
         nice -n 20 buildiso${branch} -p $profile -i $init
+        rm -f ${PROFILES}/$profile/root-overlay/etc/rc.conf
         res=$?
         stamp=$(timestamp)
         [[ $res == 0 ]]	&& { echo "$stamp == ${GREEN}Finished building ${_branch} $profile ISO with $init${ALL_OFF}" >> $CWD/ISO_build.log; } \
                         || { echo "$stamp == ${RED}Failed building   ${_branch} $profile ISO with $init${ALL_OFF}" >> $CWD/ISO_build.log; continue; }
-        rm -f ${PROFILES}/$profile/root-overlay/etc/rc.conf
         mv -v ${WORKSPACE}/iso/$profile/artix-$profile-$init-*.iso ${REPO}/
         cd $REPO && sha256sum artix-*.iso > ${REPO}/sha256sums &
     done
