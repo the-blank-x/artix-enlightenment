af889fad0d6ed2afc8466d22a80b62ce9752f16e
nous <nous@artixlinux.org>
2020-02-25 13:20:46 +0100

    Update 'buildISOs.sh'

--- buildISOs.sh
+++ buildISOs.sh
@@ -48,15 +48,15 @@ while getopts "b:p:i:" option; do
         p)
             _profile=$OPTARG
             for p in ${all_profiles[@]}; do
-				[[ ${_profile} =~ $p ]] && profiles+=($p)
-			done
+                [[ ${_profile} =~ $p ]] && profiles+=($p)
+            done
             [[ ${_profile} == all ]]    && profiles=(${all_profiles[@]})
             ;;
         i)
             _init=$OPTARG
             for i in ${all_inits[@]}; do
-				[[ ${_init} =~ $i ]] && inits+=($i)
-			done
+                [[ ${_init} =~ $i ]] && inits+=($i)
+            done
             [[ ${_init} == all ]]    && inits=(${all_inits[@]})
             ;;
     esac
@@ -64,12 +64,12 @@ done
 
 [[ $branch ]] || { _branch='stable'; branch=''; }
 [[ ${#profiles[@]} -eq 0 ]] && { echo; echo "${RED}No valid profiles selected!${ALL_OFF}"; echo; usage; }
-[[ ${#inits[@]} -eq 0 ]]	&& { echo; echo "${RED}No valid inits selected!"${ALL_OFF}; echo; usage; }
+[[ ${#inits[@]} -eq 0 ]]        && { echo; echo "${RED}No valid inits selected!"${ALL_OFF}; echo; usage; }
 
 echo "Building ISO(s):"
-echo "		branch		${BOLD}${_branch}${ALL_OFF}"
-echo "		profiles 	${GREEN}${profiles[@]}${ALL_OFF}"
-echo "		inits		${CYAN}${inits[@]}${ALL_OFF}"
+echo "          branch          ${BOLD}${_branch}${ALL_OFF}"
+echo "          profiles        ${GREEN}${profiles[@]}${ALL_OFF}"
+echo "          inits           ${CYAN}${inits[@]}${ALL_OFF}"
 
 mkdir -p ${PROFILES}
 
@@ -93,7 +93,7 @@ for profile in ${profiles[@]}; do
         rm -f ${PROFILES}/$profile/root-overlay/etc/rc.conf
         stamp=$(timestamp)
         sudo rm -fr /var/lib/artools/buildiso/$profile &
-        [[ $res == 0 ]]	&& { echo "$stamp == ${GREEN}Finished building ${_branch} $profile ISO with $init${ALL_OFF}" >> $CWD/ISO_build.log; } \
+        [[ $res == 0 ]] && { echo "$stamp == ${GREEN}Finished building ${_branch} $profile ISO with $init${ALL_OFF}" >> $CWD/ISO_build.log; } \
                         || { echo "$stamp == ${RED}Failed building   ${_branch} $profile ISO with $init${ALL_OFF}" >> $CWD/ISO_build.log; continue; }
         mv -v ${WORKSPACE}/iso/$profile/artix-$profile-$init-*.iso ${REPO}/
         cd $REPO && sha256sum artix-*.iso > ${REPO}/sha256sums &
